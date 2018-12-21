a19ea9171ca8bcef4dfaae1b582e5507d0453780
ECVD-PA-Q084-IM016 <ECVD-IM016@ECVD-PA-Q084-IM016.local>
2018-12-21 15:51:54 +0100

    Mise en place du front + animation avec données fictives

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { GameService } from './services/GameService';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
